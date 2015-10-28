6f49796c5121e973008a5d4699ec086433603645
mapineda Pineda <mapineda1222@gmail.com>
2015-10-28 06:34:13 -0500

    adding finishing touches to app

--- js/services/events.js
+++ js/services/events.js
@@ -1,2 +1,1 @@
- app.factory('events', ['$http', function($http) {return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/events-api/events.json') .success(function(data) {return data; }) .error(function(data) {return data; }); 
- }]); 
+ app.factory('events', ['$http', function($http) {return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/events-api/events.json') .success(function(data) {return data; }) .error(function(data) {return data; }); }]); 
